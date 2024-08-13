4f51528b8e5cde6cbd37828e25e33c75100d42b5
bouna <mohammed.bouna07@gmail.com>
2024-08-13 22:06:52 +0000

    add task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
+"""script for parsing web data from an api
 """
-    Uses the reddit API to print the number of subscribers of a subreddit
-"""
+import json
 import requests
-from sys import argv
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """Get the numbers of subscribers by subreddit given"""
-    url_sred_inf = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'user-agent': 'request'}
-    response = requests.get(url_sred_inf, headers=headers,
-                            allow_redirects=False)
-    if str(response) != "<Response [200]>":
+    """api call to reddit to get the number of subscribers
+    """
+    base_url = 'https://www.reddit.com/r/'
+    headers = {
+        'User-Agent': 'My-User-Agent'}
+    # grab info about all users
+    url = base_url + '{}/about.json'.format(subreddit)
+    response = requests.get(url, headers=headers)
+    resp = json.loads(response.text)
+
+    try:
+        # grab the info about the users' tasks
+        data = resp.get('data')
+        subscribers = data.get('subscribers')
+    except:
         return 0
-    r_json = response.json()
-    num_subs = r_json.get("data").get("subscribers")
-    return num_subs
+    if subscribers is None:
+        return 0
+    return int(subscribers)
