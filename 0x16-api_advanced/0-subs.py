73ff445c42fc5f8a369ed7a7f883809866d0dc29
bouna <mohammed.bouna07@gmail.com>
2024-08-13 21:49:22 +0000

    add task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
+"""Module for task 0"""
+
 import requests
 
-
 def number_of_subscribers(subreddit):
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = headers = {'User-Agent': 'My-User-Agent'}
-    with requests.session() as client:
-        info = client.get(url, headers=headers, allow_redirects=False).json()
-        try:
-            return info.get('data', {}).get('subscribers', 0)
-        except Exception:
-            return 0
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "My-User-Agent"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    if response.status_code >= 300:
+        return 0
+    
+    data = response.json().get("data", {})
+    return data.get("subscribers", 0)
