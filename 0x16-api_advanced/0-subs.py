0ee4a0dfe85eb6b643b8107f4e23f3033a96f56a
bouna <mohammed.bouna07@gmail.com>
2024-08-14 15:02:46 +0000

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
 """
-a simple function to get number of subs of subreddit
+A simple function to get the number of subscribers of a subreddit.
 """
 import requests
 
-client_id = "vCoCeS5JDM_uX__qWq9dLA"
-secret = "Xv4PSvskDSyCA_O0tLULNOjr53fQcg"
-auth = requests.auth.HTTPBasicAuth(client_id, secret)
-data = {
+def number_of_subscribers(subreddit):
+    """A function to start sending requests to the API."""
+    client_id = "vCoCeS5JDM_uX__qWq9dLA"
+    secret = "Xv4PSvskDSyCA_O0tLULNOjr53fQcg"
+    auth = requests.auth.HTTPBasicAuth(client_id, secret)
+    data = {
         'grant_type': 'password',
         'username': 'Majestic_Bluebird490',
         'password': "0633barcelone@M"
     }
-headers = {'User-Agent': 'MyAPI'}
-res = requests.post("https://www.reddit.com/api/v1/access_token",
-                    auth=auth, data=data, headers=headers)
-token = res.json()['access_token']
-headers['Authorization'] = 'bearer {}'.format(token)
+    headers = {'User-Agent': 'MyAPI'}
+    res = requests.post("https://www.reddit.com/api/v1/access_token",
+                        auth=auth, data=data, headers=headers)
+    print(res)
+    token = res.json()['access_token']
+    headers['Authorization'] = 'bearer {}'.format(token)
 
-
-def number_of_subscribers(subreddit):
-    """ a function to startsending requests to API"""
-    test = requests.get('https://oauth.reddit.com/r/{}/about'
-                        .format(subreddit), headers=headers)
-    if test.status_code == 200:
-        return (test.json()["data"]["subscribers"])
+    response = requests.get('https://oauth.reddit.com/r/{}/about'.format(subreddit), headers=headers)
+    if response.status_code == 200:
+        return response.json()["data"]["subscribers"]
     else:
         return 0
