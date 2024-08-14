d230b7b59ca78fcc58a359de7a9fa5f733383838
bouna <mohammed.bouna07@gmail.com>
2024-08-14 15:17:57 +0000

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
 """
-A simple function to get the number of subscribers of a subreddit.
+a simple function to get number of subs of subreddit
 """
 import requests
 
-def number_of_subscribers(subreddit):
-    """A function to start sending requests to the API."""
-    client_id = "vCoCeS5JDM_uX__qWq9dLA"
-    secret = "Xv4PSvskDSyCA_O0tLULNOjr53fQcg"
-    auth = requests.auth.HTTPBasicAuth(client_id, secret)
-    data = {
+client_id = "vCoCeS5JDM_uX__qWq9dLA"
+secret = "Xv4PSvskDSyCA_O0tLULNOjr53fQcg"
+auth = requests.auth.HTTPBasicAuth(client_id, secret)
+data = {
         'grant_type': 'password',
         'username': 'Majestic_Bluebird490',
         'password': "0633barcelone@M"
     }
-    headers = {'User-Agent': 'MyAPI'}
-    res = requests.post("https://www.reddit.com/api/v1/access_token",
-                        auth=auth, data=data, headers=headers)
+headers = {'User-Agent': 'MyAPI'}
+res = requests.post("https://www.reddit.com/api/v1/access_token",
+                    auth=auth, data=data, headers=headers)
+token = res.json()['access_token']
+headers['Authorization'] = 'bearer {}'.format(token)
 
-    token = res.json()['access_token']
-    headers['Authorization'] = 'bearer {}'.format(token)
 
-    response = requests.get('https://oauth.reddit.com/r/{}/about'.format(subreddit), headers=headers)
-    if response.status_code == 200:
-        return response.json()["data"]["subscribers"]
-    else:
+def number_of_subscribers(subreddit):
+    """ a function to startsending requests to API"""
+    test = requests.get('https://oauth.reddit.com/r/{}/about'
+                        .format(subreddit), headers=headers)
+    if test.status_code != 200:
         return 0
+    json_response = test.json()
+    if 'data' in json_response and 'subscribers' in json_response['data']:
+        return json_response['data']['subscribers']
+    return 0
