b013bd6c13aaf6ba506ecf7c8acc41436948bece
bouna <mohammed.bouna07@gmail.com>
2024-08-14 16:27:20 +0000

    test

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,33 +1,35 @@
-#!/usr/bin/python3
-"""
-a simple function to get top 10 hot posts of a subreddit
-"""
 import requests
 
 client_id = "vCoCeS5JDM_uX__qWq9dLA"
 secret = "Xv4PSvskDSyCA_O0tLULNOjr53fQcg"
 auth = requests.auth.HTTPBasicAuth(client_id, secret)
 data = {
-        'grant_type': 'password',
-        'username': 'Majestic_Bluebird490',
-        'password': "0633barcelone@M"
-        }
+    'grant_type': 'password',
+    'username': 'Majestic_Bluebird490',
+    'password': "0633barcelone@M"
+}
 headers = {'User-Agent': 'MyAPI'}
 res = requests.post("https://www.reddit.com/api/v1/access_token",
                     auth=auth, data=data, headers=headers)
-token = res.json()['access_token']
+token = res.json().get('access_token')
 headers['Authorization'] = 'bearer {}'.format(token)
 
 
 def top_ten(subreddit):
-    """ a function to startsending requests to API"""
+    """ a function to start sending requests to API"""
     test = requests.get('https://oauth.reddit.com/r/{}/hot'
                         .format(subreddit), headers=headers)
     if test.status_code == 200:
-        res = []
-        for post in test.json()["data"]["children"]:
-            res.append(post["data"]["title"])
-        for i in range(10):
-            print(res[i])
+        try:
+            json_data = test.json()
+            res = []
+            for post in json_data["data"]["children"]:
+                res.append(post["data"]["title"])
+            for i in range(10):
+                print(res[i])
+        except ValueError:
+            print("Error: Response is not valid JSON")
+            return 0
     else:
+        print("Error: Status code is not 200")
         return 0
